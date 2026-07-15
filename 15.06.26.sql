3f947645a74a60852e8374ad1c591da1695125d6
NEVIL <147979766+nevil2006@users.noreply.github.com>
2026-07-15 09:16:04 +0530

    Create CollegeDB with Students, Books, and Courses tables

---
+++ 15.06.26.sql
@@ -0,0 +1,25 @@
+1.CREATE DATABASE CollegeDB;
+USE CollegeDB;
+
+
+2.CREATE TABLE Students (
+    student_id INT,
+    name VARCHAR(50),
+    age INT
+);
+
+
+3.CREATE TABLE Books (
+    book_id INT,
+    title VARCHAR(50),
+	author VARCHAR(50),
+    price INT
+);
+
+
+4.CREATE TABLE Courses (
+    course_id INT,
+    course_name VARCHAR(50),
+    duration INT
+);
+
